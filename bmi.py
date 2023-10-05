463c03968163c569b2ce4d6e4c331b1524a26e50
samanritgithub <ssamankumari6@gmail.com>
2023-10-05 14:13:09 +0530

    update

--- bmi.py
+++ bmi.py
@@ -6,7 +6,11 @@ from PIL import Image,ImageTk
 
 root= Tk()
 
-
+root= Tk()
+root.title('BMI calculator')
+root.geometry('470x580+300+200')
+root.resizable(False, False)
+root.configure(bg="#f0f1f5")
 
 
 
